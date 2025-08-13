dd85af4c0ac602093f502fd59eeca5377de7ea92
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2025-08-13 14:47:41 +0300

    Better tests,and now the id is passed when the card is created

--- src/main/java/top/barakaon/stingy_family/CashCardController.java
+++ src/main/java/top/barakaon/stingy_family/CashCardController.java
@@ -3,13 +3,16 @@ package top.barakaon.stingy_family;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/cashcards")
 public class CashCardController {
-    @GetMapping("/cashcards/{id}")
+    @GetMapping("/{id}")
     private ResponseEntity<CashCard> findCashCardById(@PathVariable Long id){
-      CashCard cashCard = null;
+      CashCard cashCard = new CashCard(id,38900,"233gdhbcyjgeychgeye");
+
       return ResponseEntity.ok(cashCard);
     }
 }
